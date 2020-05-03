61ef9990ab22b2362e31209076384683c45da8cb
Yuri Reis <yuri@yurireis.com>
2020-05-02 22:46:19 -0300

    Criação da app website, adicionado arquivos statics, e direcionamento de alguns templates de testes

--- apps/empresas/views.py
+++ apps/empresas/views.py
@@ -1,3 +1,9 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+@login_required
+def home(request):
+    return HttpResponse('Olá')
+
